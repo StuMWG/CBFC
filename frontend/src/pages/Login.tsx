dce804db19c1c61a3d8c96db3d5431393f469e43
StuMWG <113923562+StuMWG@users.noreply.github.com>
2025-03-19 13:44:27 -0500

    Update

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Card } from "react-bootstrap";
+import CustomButton from "../components/Button";
+
+const Login: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Redirect to Dashboard
+    navigate("/dashboard");
+  };
+  const handleRegistration = () => {
+    // Redirect to Registration
+    navigate("/registration");
+  };
+
+  return (
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Card style={{ width: "25rem", padding: "20px" }}>
+        <h2 className="text-center">Login</h2>
+        {/* Login Form Here */}
+        <CustomButton label="Login" type="button" onClick={handleLogin} fullWidth />
+      </Card>
+      <Card style={{ width: "25rem", padding: "20px" }}>
+        <h2 className="text-center">Registration</h2>
+        {/* Login Form Here */}
+        <CustomButton label="Register" type="button" onClick={handleRegistration} fullWidth />
+      </Card>
+    </Container>
+  );
+};
+
+export default Login;
