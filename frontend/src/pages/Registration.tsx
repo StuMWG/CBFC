5dd8d999c9ffe03f776d40ebe3914ca6a5ab9495
StuMWG <113923562+StuMWG@users.noreply.github.com>
2025-03-20 14:14:46 -0500

    Update
    Added page navigation between login and register
    Added buttons and text fields

--- frontend/src/pages/Registration.tsx
+++ frontend/src/pages/Registration.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import CustomButton from "../components/Button";
+import TextField from "../components/InputField";
+import "../styles/Registration.css";
 
 const Registration: React.FC = () => {
+
+  const navigate = useNavigate();
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordVerify, verifyPassword] = useState<string>("");
+
+  const handleSubmit = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>Registration</h1>
+    <div className="fullscreen-container">
+      <Card className="shadow-lg p-4" style={{ width: "22rem" }}>
+        <h2 className="text-center mb-4">Login</h2>
+        <TextField label="Username" placeholder="Enter a username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} fullWidth/>
+        <TextField label="Email" placeholder="Enter your email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} fullWidth/>
+        <TextField label="Password" placeholder="Enter a password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} fullWidth/>
+        <TextField label="Verify Password" placeholder="Enter password" type="password" value={passwordVerify} onChange={(e) => verifyPassword(e.target.value)} fullWidth/>
+        <CustomButton label="Submit" type="submit" onClick={handleSubmit} fullWidth />
+      </Card>
     </div>
-  );
+    );
 };
 
 export default Registration;
